88dcae6021897c80bee5b1a2a71e7cdb455ab409
FATIMABAKARROUCH <156421741+FATIMABAKARROUCH@users.noreply.github.com>
2024-01-29 15:08:54 +0100

    :test git

---
+++ session-01/index.js
@@ -0,0 +1,51 @@
+//console.log("hello world")//
+/*
+MULTILIGNES
+*/
+/*let nombreUn;
+let nombreDeux;
+let somme;
+const PI = 3.14;
+nombreUn = 20;
+nombreDeux = 15;
+somme = nombreUn + nombreDeux;
+*/
+//console.log(somme)
+// les fonction
+/*function greet(nom){
+    console.log("helloooo!" + nom)
+}
+greet("bakarrouch")*/
+ /*function somme(a,b){
+     S=a+b
+     return(S)
+
+}
+console.log(somme(10,20))
+/*
+
+
+
+
+/*let a;
+let i;
+i = 0
+for i in Range(1,5);
+ fact = i*a
+console.log(fact)*/
+
+
+function inscrire(){
+    let nom = "bakarrouch"
+    let prenom = "fatima"
+    let age= 19
+
+    if (age == 19){
+        let statut = "etudiante"
+        console.log(statut)
+    }
+}
+inscrire()
+
+      
+    
---
+++ session-02/index2.js
@@ -0,0 +1,15 @@
+/*let a =10;
+let b ="10"
+if(a!=b){
+    console.log("yeees")
+}else{
+    console.log("noooo")
+};
+let statut= "etudiant";
+let age=20;
+
+if (statut)*/
+ let valid=false;
+ if (!valid){
+    console.log("this one doesn't exist")
+ };
